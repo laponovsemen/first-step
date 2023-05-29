d5fdab929b3ad190dd1cd0bfc85d0f11c94b4bea
Laponov Semen <johndoe@example.com>
2023-05-29 17:42:14 +0300

    delete everithing

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,7 @@ import { BlogsService } from './blogs/blogs.service';
 import { BlogsRepository } from './blogs/blogs.repository';*/
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://simsbury65:incubator@cluster0.vai5lbz.mongodb.net/?retryWrites=true&w=majority',
-    ),
-
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
