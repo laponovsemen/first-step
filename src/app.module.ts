f6e57a38bbe4265ac0815393484daaa892589d92
Laponov Semen <johndoe@example.com>
2023-05-29 18:57:34 +0300

    start making nestjs app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-/*import { BlogsController } from './blogs/blogs.controller';
+import { ConfigModule } from '@nestjs/config';
+import { BlogsController } from './blogs/blogs.controller';
 import { BlogsService } from './blogs/blogs.service';
-import { BlogsRepository } from './blogs/blogs.repository';*/
+import { BlogsRepository } from './blogs/blogs.repository';
+import * as process from "process";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot(),
+  MongooseModule.forRoot(process.env.MONGO_URL)],
+  controllers: [AppController,BlogsController],
+  providers: [AppService, BlogsService, BlogsRepository],
 })
 export class AppModule {}
