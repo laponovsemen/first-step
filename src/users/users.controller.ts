a6d02e79f22098f34ff7b15fc45ab2fde59ec6d2
Laponov Semen <johndoe@example.com>
2023-05-31 04:31:02 +0300

    making testing router and all repos

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,6 +9,7 @@ class UserDTO {
   @Length(3, 10)
   @Matches(/^[a-zA-Z0-9_-]*$/)
   login : string //maxLength: 10 minLength: 3 pattern: ^[a-zA-Z0-9_-]*$
+
   @IsNotEmpty()
   @Length(6, 20)
   password: string // maxLength: 20 minLength: 6
