319dd8066abcd9dabd39db3fa261ae9a06badd36
Laponov Semen <johndoe@example.com>
2023-07-09 21:38:24 +0300

    Revert "fixing bugs with wrong return of mongo Comment and Post splice"
    This reverts commit a1a23b384220ebeea9c30cb99b43ced2b6205c91.

--- src/app.module.ts
+++ src/app.module.ts
@@ -93,7 +93,7 @@ const adapters = [EmailAdapter, Common, BlogIdExistsRule]
     JwtModule.register({secret: "123"}),
     ThrottlerModule.forRoot({
     ttl: 10,
-    limit: 5,
+    limit: 500,
     }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URL!),
